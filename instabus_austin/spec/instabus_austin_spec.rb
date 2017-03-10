0a24732722862fbfbd775ee601ca35f5729baaed
larry-cherry <larry.cherry.0406@gmail.com>
2017-03-09 21:29:02 -0600

    wrote test for instabus latitude and longitude for stops

--- instabus_austin/spec/instabus_austin_spec.rb
+++ instabus_austin/spec/instabus_austin_spec.rb
@@ -1,11 +1,25 @@
 require "spec_helper"
+require "pry"
 
 RSpec.describe InstabusAustin do
   it "has a version number" do
     expect(InstabusAustin::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(InstabusAustin.stops(1,0)).to_not eq(nil)
+  describe "Checks Bus Stop Method" do
+
+    it "Recieve a response" do
+      expect(InstabusAustin.stops(1,0)).to_not eq(nil)
+  	end
+
+  	it "Checks if stop_lat is a float" do 
+  		expect(InstabusAustin.stops(1,0).first["stop_lat"]).to be_a Float
+  	end
+
+  	it "Checks if stop_lon is a float" do 
+  		expect(InstabusAustin.stops(1,0).first["stop_lon"]).to be_a Float
+  	end
+
   end
+
 end
